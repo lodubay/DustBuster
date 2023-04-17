cd202eba4c5ad61572449d41dc3cdd74a498dff3
Liam Dubay <liam.dubay@gmail.com>
2023-04-17 11:17:59 -0400

    add 2d line-of-sight projections

---
+++ _globals.py
@@ -0,0 +1,17 @@
+"""
+Global parameters
+"""
+
+# Cygnus OB2 sky coordinates in degrees
+CygOB2_ra = 308.317
+CygOB2_dec = 41.32
+
+# Cygnus OB2 distance in parsecs
+# Kharchenko et al. (2013)
+CygOB2_dist = 1593
+
+# Position of the Sun in data units
+sun_pos = (300.5, 300.5, 40.5)
+
+# Physical gridstep size in pc
+gridstep_pc = 10
